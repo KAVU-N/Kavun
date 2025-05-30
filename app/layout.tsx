6cb779cbff065c18ddf7eea5ef49572710901fea
Anas Bashir <nasbas23@gmail.com>
2025-05-30 22:47:46 +0300

    Remove invalid NextJs variable

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,6 @@ export const metadata = {
 };
 
 export const viewport = 'width=device-width, initial-scale=1, maximum-scale=1';
-export const themeColor = '#ffffff';
 
 export default function RootLayout({
   children,
